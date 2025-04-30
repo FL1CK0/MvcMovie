c56d2da26cec247b56aec11b30037324d77c9aae
kille <kille@ECHIPSCOMPUTER>
2025-04-30 13:52:38 +0400

    Добавьте файлы проекта.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "Привет всем, я Конор и я проверю как это работает на самом деле!";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "Пока мир";
+    }
+}
